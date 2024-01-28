a72ced83e566ac166bc1dcc88a4b4e3a992ace66
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2024-01-28 11:52:29 +0800

    State Machine Getters * Added 'GetPlayback' and 'GetKeyPlayback' to Animator * Added optimisation attributes to some methods * Updated brace positioning to match current style

--- Animator.cs
+++ Animator.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Godot;
 
 namespace SadChromaLib.Animation;
@@ -16,28 +17,26 @@ public sealed class Animator
 
     #region Setters
 
-    public void SetTimeScale(StringName key, float scale)
-    {
+    public void SetTimeScale(StringName key, float scale) {
         _tree.Set(key, scale);
     }
 
-    public void SetBlend(StringName key, float position)
-    {
+    public void SetBlend(StringName key, float position) {
         _tree.Set(key, position);
     }
 
-    public void SetBlend(StringName key, Vector2 position)
-    {
+    public void SetBlend(StringName key, Vector2 position) {
         _tree.Set(key, position);
     }
 
-    public void SetOneShot(StringName key, AnimationNodeOneShot.OneShotRequest request)
+    public void SetOneShot(
+        StringName key,
+        AnimationNodeOneShot.OneShotRequest request)
     {
         _tree.Set(key, (int) request);
     }
 
-    public void SetState(StringName key, string value)
-    {
+    public void SetState(StringName key, string value) {
         _tree.Set(key, value);
     }
 
@@ -45,46 +44,66 @@ public sealed class Animator
 
     #region Blenders
 
-    public void BlendFloat(StringName key, float value, float fac)
-    {
+    public void BlendFloat(StringName key, float value, float fac) {
         _tree.Set(key, Mathf.Lerp((float) _tree.Get(key), value, fac));
     }
 
-    public void BlendPosition(StringName key, Vector2 value, float fac)
-    {
+    public void BlendPosition(StringName key, Vector2 value, float fac) {
         _tree.Set(key, ((Vector2) _tree.Get(key)).Lerp(value, fac));
     }
 
-    public void BlendTimeScale(StringName key, float scale, float fac) => BlendFloat(key, scale, fac);
-    public void BlendInterpolated(StringName key, float position, float fac) => BlendFloat(key, position, fac);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void BlendTimeScale(StringName key, float scale, float fac)
+        => BlendFloat(key, scale, fac);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void BlendInterpolated(StringName key, float position, float fac)
+        => BlendFloat(key, position, fac);
+
+    #endregion
+
+    #region Getters
+
+    /// <summary>
+    /// Must only be called on a wrapped AnimationTree with a state machine as its root.
+    /// </summary>
+    /// <returns></returns>
+    public AnimationNodeStateMachinePlayback GetPlayback() {
+        return (AnimationNodeStateMachinePlayback) _tree.Get("parameters/playback");
+    }
 
     #endregion
 
     #region Key Getters
 
-    public static StringName GetKeyBlendPosition(string keyName)
-    {
-        return new($"parameters/{keyName}/blend_position");
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static StringName GetKeyBlendPosition(string keyName) {
+        return $"parameters/{keyName}/blend_position";
     }
 
-    public static StringName GetKeyBlendAmount(string keyName)
-    {
-        return new($"parameters/{keyName}/blend_amount");
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static StringName GetKeyBlendAmount(string keyName) {
+        return $"parameters/{keyName}/blend_amount";
     }
 
-    public static StringName GetKeyTimeScale(string keyName)
-    {
-        return new($"parameters/{keyName}/scale");
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static StringName GetKeyTimeScale(string keyName) {
+        return $"parameters/{keyName}/scale";
     }
 
-    public static StringName GetKeyTransition(string keyName)
-    {
-        return new($"parameters/{keyName}/transition_request");
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static StringName GetKeyTransition(string keyName) {
+        return $"parameters/{keyName}/transition_request";
     }
 
-    public static StringName GetKeyOneShot(string keyName)
-    {
-        return new($"parameters/{keyName}/request");
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static StringName GetKeyPlayback(string keyName) {
+        return $"parameters/{keyName}/playback";
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static StringName GetKeyOneShot(string keyName) {
+        return $"parameters/{keyName}/request";
     }
 
     #endregion
